c70cd96d2305d4a69df9de629691261802f73fe1
Mohamed Mumen <mmumen102@gmail.com>
2020-06-05 10:33:00 +0100

    new HTML & JavaSrcipt page

---
+++ home.js
@@ -0,0 +1,10 @@
+let random = Math.floor((Math.random() * 2)) ;
+function heads(){ 
+    if (random => 2){
+        document.getElementById('coin').innerHTML = random;
+        document.getElementById('status').innerHTML = "you won congratulation"
+    } else {
+        document.getElementById('coin').innerHTML = random;
+        document.getElementById('status').innerHTML = "You Lose. Glory to our site";
+    }
+}
