f6a4703c3ff9b59089181f174a5c167e153055dd
Mohamed Mumen <mmumen102@gmail.com>
2020-06-08 00:24:15 +0100

    javascript for game 4 boxes

---
+++ four.js
@@ -0,0 +1,55 @@
+let random = Math.floor((Math.random() * 100)) ;
+function first(){ 
+    if (random <= 25){
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "you won, how unfortunate";
+        location.replace("page.html")
+        console.log(random);
+        
+    } else {
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "You Lose. Glory to our site";
+        console.log(random)
+    }
+}
+
+function second() {
+    if (random => 26 && random <=50){
+        /*document.getElementById('coin').innerHTML = random*/
+        document.getElementById('status').innerHTML = "you won congratulation";
+         location.replace("page.html")
+        console.log(random)
+    } else {
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "You Lose. Glory to our site";
+        console.log(random)
+    }
+}
+
+function third(){ 
+    if (random => 51 && random <= 75){
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "you won, how unfortunate";
+        location.replace("page.html")
+        console.log(random);
+        
+    } else {
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "You Lose. Glory to our site";
+        console.log(random)
+    }
+}
+
+function fourth(){ 
+    if (random => 76){
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "you won, how unfortunate";
+        location.replace("page.html")
+        console.log(random);
+        
+    } else {
+        /*document.getElementById('coin').innerHTML = random;*/
+        document.getElementById('status').innerHTML = "You Lose. Glory to our site";
+        console.log(random)
+    }
+}
